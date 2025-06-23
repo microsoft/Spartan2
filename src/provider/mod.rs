e406283659f4c1de149076086629aae3caa65ec3
Srinath Setty <srinath@microsoft.com>
2025-06-23 13:32:41 -0700

    add engines for IPA

--- src/provider/mod.rs
+++ src/provider/mod.rs
@@ -21,21 +21,22 @@ use crate::{
   },
   traits::Engine,
 };
+use core::fmt::Debug;
 use serde::{Deserialize, Serialize};
 
-/// An implementation of the Spartan `Engine` trait with Pallas curve and Hyrax commitment scheme
+/// An implementation of the Spartan Engine trait with Pallas curve and Hyrax commitment scheme
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub struct PallasHyraxEngine;
 
-/// An implementation of the Spartan `Engine` trait with Vesta curve and Hyrax commitment scheme
+/// An implementation of the Spartan Engine trait with Vesta curve and Hyrax commitment scheme
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub struct VestaHyraxEngine;
 
-/// An implementation of the Spartan `Engine` trait with P256 curve and Hyrax commitment scheme
+/// An implementation of the Spartan Engine trait with P256 curve and Hyrax commitment scheme
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub struct P256HyraxEngine;
 
-/// An implementation of the Spartan `Engine` trait with T256 curve and Hyrax commitment scheme
+/// An implementation of the Spartan Engine trait with T256 curve and Hyrax commitment scheme
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub struct T256HyraxEngine;
 
@@ -75,19 +76,19 @@ impl Engine for T256HyraxEngine {
   type EE = HyraxEvaluationEngine<Self>;
 }
 
-/// An implementation of the Spartan `Engine` trait with Pallas curve and IPA PCS
+/// An implementation of the Spartan Engine trait with Pallas curve and IPA PCS
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub struct PallasIPAEngine;
 
-/// An implementation of the Spartan `Engine` trait with Vesta curve and IPA PCS
+/// An implementation of the Spartan Engine trait with Vesta curve and IPA PCS
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub struct VestaIPAEngine;
 
-/// An implementation of the Spartan `Engine` trait with P256 curve and IPA PCS
+/// An implementation of the Spartan Engine trait with P256 curve and IPA PCS
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub struct P256IPAEngine;
 
-/// An implementation of the Spartan `Engine` trait with T256 curve and IPA PCS
+/// An implementation of the Spartan Engine trait with T256 curve and IPA PCS
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub struct T256IPAEngine;
 
@@ -125,47 +126,4 @@ impl Engine for T256IPAEngine {
   type TE = Keccak256Transcript<Self>;
   type CE = IPACommitmentEngine<Self>;
   type EE = IPAEvaluationEngine<Self>;
-}
-
-#[cfg(test)]
-mod tests {
-  use crate::provider::{pasta::pallas, traits::DlogGroup};
-  use digest::{ExtendableOutput, Update};
-  use halo2curves::{CurveExt, group::Curve};
-  use sha3::Shake256;
-  use std::io::Read;
-
-  macro_rules! impl_cycle_pair_test {
-    ($curve:ident) => {
-      fn from_label_serial(label: &'static [u8], n: usize) -> Vec<$curve::Affine> {
-        let mut shake = Shake256::default();
-        shake.update(label);
-        let mut reader = shake.finalize_xof();
-        (0..n)
-          .map(|_| {
-            let mut uniform_bytes = [0u8; 32];
-            reader.read_exact(&mut uniform_bytes).unwrap();
-            let hash = $curve::Point::hash_to_curve("from_uniform_bytes");
-            hash(&uniform_bytes).to_affine()
-          })
-          .collect()
-      }
-
-      let label = b"test_from_label";
-      for n in [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 1021,
-      ] {
-        let ck_par = <$curve::Point as DlogGroup>::from_label(label, n);
-        let ck_ser = from_label_serial(label, n);
-        assert_eq!(ck_par.len(), n);
-        assert_eq!(ck_ser.len(), n);
-        assert_eq!(ck_par, ck_ser);
-      }
-    };
-  }
-
-  #[test]
-  fn test_pallas_from_label() {
-    impl_cycle_pair_test!(pallas);
-  }
 }
--- src/provider/pasta.rs
+++ src/provider/pasta.rs
@@ -46,3 +46,46 @@ impl_traits!(
   "40000000000000000000000000000000224698fc094cf91b992d30ed00000001",
   "40000000000000000000000000000000224698fc0994a8dd8c46eb2100000001"
 );
+
+#[cfg(test)]
+mod tests {
+  use crate::provider::{pasta::pallas, traits::DlogGroup};
+  use digest::{ExtendableOutput, Update};
+  use halo2curves::{CurveExt, group::Curve};
+  use sha3::Shake256;
+  use std::io::Read;
+
+  macro_rules! impl_cycle_pair_test {
+    ($curve:ident) => {
+      fn from_label_serial(label: &'static [u8], n: usize) -> Vec<$curve::Affine> {
+        let mut shake = Shake256::default();
+        shake.update(label);
+        let mut reader = shake.finalize_xof();
+        (0..n)
+          .map(|_| {
+            let mut uniform_bytes = [0u8; 32];
+            reader.read_exact(&mut uniform_bytes).unwrap();
+            let hash = $curve::Point::hash_to_curve("from_uniform_bytes");
+            hash(&uniform_bytes).to_affine()
+          })
+          .collect()
+      }
+
+      let label = b"test_from_label";
+      for n in [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 1021,
+      ] {
+        let ck_par = <$curve::Point as DlogGroup>::from_label(label, n);
+        let ck_ser = from_label_serial(label, n);
+        assert_eq!(ck_par.len(), n);
+        assert_eq!(ck_ser.len(), n);
+        assert_eq!(ck_par, ck_ser);
+      }
+    };
+  }
+
+  #[test]
+  fn test_pallas_from_label() {
+    impl_cycle_pair_test!(pallas);
+  }
+}
