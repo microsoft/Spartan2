a14ac4b0cc89faa0479b38ae8afaca3c47b9a845
Srinath Setty <srinath@microsoft.com>
2025-07-30 14:41:58 -0700

    increase width

--- src/provider/pcs/hyrax_pc.rs
+++ src/provider/pcs/hyrax_pc.rs
@@ -98,7 +98,7 @@ where
   type EvaluationArgument = HyraxEvaluationArgument<E>;
 
   fn width() -> usize {
-    32768 // Hyrax PC is always 1024 columns wide
+    8192 // Hyrax PC is always 8192 columns wide
   }
 
   /// Derives generators for Hyrax PC, where num_vars is the number of variables in multilinear poly
